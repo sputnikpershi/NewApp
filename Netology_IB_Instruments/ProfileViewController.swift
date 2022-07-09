91ef4ad5d526eb7f0fcb936f304e914fb16581a8
Kiryl <sputnik.pershi@ya.ru>
2022-07-09 17:24:22 +0800

    7.Создайте новый файл Profile.storyboard. 8. Создайте новый файл ProfileViewController.swift, унаследуйтесь от класса UIViewController.

---
+++ Netology_IB_Instruments/ProfileViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileViewController.swift
+//  Netology_IB_Instruments
+//
+//  Created by Krime Loma    on 7/9/22.
+//
+
+import Foundation
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+}
